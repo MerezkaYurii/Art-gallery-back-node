d0fa6826cd0614492a7df368b210b4c633ead0ce
MerezkaYurii <shkolasad@ukr.net>
2025-08-20 17:50:18 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // src/index.js
 
-import { initMongoDB } from './db/initMongoDB.js';
+import { initMongoDB } from './src/db/initMongoDB.js';
 import { startServer } from './server.js';
 
 const bootstrap = async () => {
